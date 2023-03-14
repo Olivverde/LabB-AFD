21c3029df23ab6d40e04476bd8c2b779ad9244a0
Olivverde <dele19270@uvg.edu.gt>
2023-03-14 00:18:21 -0600

    AFD Direct Construction - 33% Completed

--- labB.py
+++ labB.py
@@ -88,7 +88,6 @@ class FDA(object):
             states_list.pop('ε')
             states_cont += 1
 
-
         for i in afd_table:
             for e in afd_table[i]:
                 for t in trans_table:
@@ -98,7 +97,136 @@ class FDA(object):
         print('all_states',all_states)
         print('afd_table',afd_table)
 
-re_list = ['(a|b)*abb']
+    def dstates_recursive(self, table, dstate_table, fp, index, sym_list):
+        new_states = []
+        aux_dstates = []
+        dstate_table[index] = {}
+        for i in sym_list:
+            for j in fp:
+                if table[j]['symbol'] == i: 
+                    aux_dstates = list(set(aux_dstates) | set(table[j]['followpos']))
+            dstate_table[index][i] = aux_dstates
+            new_states.append(aux_dstates)
+            aux_dstates = []
+        
+        return dstate_table, new_states
+    
+    def regular_toAFD(self, r):
+        
+        iteration = len(r)
+        fda_stack = [char for char in r]
+        index = []
+        regular_table = {}
+        cont = 0
+        cont_nonsy = 0
+        root = ''
+
+        for i in range(iteration):
+            e = fda_stack.pop(0)
+            pos_template = {'nullable':None,'firstpos': [], 'lastpos':[],
+                         'followpos':[], 'symbol':''}
+            if e in ['.','|','*']:
+                cont_nonsy += 1
+                non_index = 'r'+str(cont_nonsy)
+                root = non_index
+                index.append(non_index)
+                regular_table[non_index] = pos_template
+            else:
+                cont += 1
+                index.append(cont)
+                regular_table[cont] = pos_template
+                
+            if e == '.':
+                pass
+                e = index[i]
+                c1 = index[i-2]
+                c2 = index[i-1]
+                c1_nullable = regular_table[c1]['nullable']
+                c2_nullable = regular_table[c2]['nullable']
+                
+                regular_table[e]['nullable'] = c1_nullable and c2_nullable
+
+                
+                if c1_nullable:
+                    regular_table[e]['firstpos'] = list(set(regular_table[c1]['firstpos'])|set(regular_table[c2]['firstpos']))
+                else:
+                    regular_table[e]['firstpos'] = regular_table[c1]['firstpos']
+                    
+                if c2_nullable:
+                    regular_table[e]['lastpos'] = list(set(regular_table[c1]['lastpos'])|set(regular_table[c2]['lastpos']))
+                else:
+                    regular_table[e]['lastpos'] = regular_table[c2]['lastpos']
+
+                for i in regular_table[c1]['lastpos']:
+                    first_c2 = regular_table[c2]['firstpos']
+                    follow_i = regular_table[i]['followpos']
+                    union = list(set(follow_i)|set(first_c2))
+                    regular_table[i]['followpos'] = union
+                
+            elif e == '|':
+                
+                e = index[i]
+                c1 = index[i-2]
+                c2 = index[i-1]
+                
+                regular_table[e]['nullable'] = regular_table[c1]['nullable'] or regular_table[c2]['nullable']
+                regular_table[e]['firstpos'] = list(set(regular_table[c1]['firstpos'])|set(regular_table[c2]['firstpos']))
+                regular_table[e]['lastpos'] = list(set(regular_table[c1]['lastpos'])|set(regular_table[c2]['lastpos']))
+                                 
+            elif e == '*':
+                e = index[i]
+                c1 = index[i-1]
+                regular_table[e]['nullable'] = True
+                regular_table[e]['firstpos'] = regular_table[c1]['firstpos']
+                regular_table[e]['lastpos'] = regular_table[c1]['lastpos']
+                
+                for i in regular_table[e]['lastpos']:
+                    first_n = regular_table[e]['firstpos']
+                    follow_i = regular_table[i]['followpos']
+                    union = list(set(follow_i)|set(first_n))
+                    regular_table[i]['followpos'] = union
+                
+            else:  
+                sym = e
+                e = index[i]
+                if e == 'ε':
+                    regular_table[e]['nullable'] = True
+                
+                else:
+                    regular_table[e]['nullable'] = False
+                    regular_table[e]['firstpos'].append(e)
+                    regular_table[e]['lastpos'].append(e)
+                    regular_table[e]['symbol'] = sym  
+        
+        sym_set = set()
+        for inner_dicc in regular_table.values():
+            symbol = inner_dicc.get("symbol")
+            if symbol and symbol.strip():
+                sym_set.add(symbol.strip())
+        sym_list = sorted(list(sym_set))
+        sym_list.remove('#')
+        
+        dstates = [regular_table[root]['firstpos']]
+        dstates_table = {}
+        new_states = []
+        for fp in dstates:
+            fp_ind = dstates.index(fp)
+            dstates_table, new_states = self.dstates_recursive(regular_table, dstates_table, fp, fp_ind, sym_list)
+            
+            for ns in new_states:
+                if not ns in dstates:
+                    dstates.append(ns)
+        
+        for i in sym_list:
+            for fp in dstates_table:
+                stt = dstates_table[fp][i]
+                dstates_table[fp][i] = dstates.index(stt)
+                
+        
+        print(dstates_table)
+
+
+re_list = ['(a|b)*abb#']
 re = re_list[0]
 
 lib = Libs(re)
@@ -110,11 +238,13 @@ postfix = lib.get_postfix()
 
 nfa = NFA()
 fda = FDA()
+postfix = 'ab|*a.b.b.'
+postfix_fda = 'ab|*a.b.b.#.'
 nfa.thompson(postfix)
 # states, nodes, transitions, etc. NFA in a nutshell
 transitions = nfa.get_transitions()
 acc_st = nfa.get_acceptance_state()+1
 
-fda.subConstruct(transitions, acc_st)
-            
+# fda.subConstruct(transitions, acc_st)
+fda.regular_toAFD(postfix_fda)
         
